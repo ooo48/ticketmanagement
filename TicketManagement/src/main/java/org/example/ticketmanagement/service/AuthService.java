8af347e135c34371a28c492860b96e2c502709ad
alll <2966801095@qq.com>
2025-11-30 16:31:12 +0800

    Delete TicketManagement directory

--- TicketManagement/src/main/java/org/example/ticketmanagement/service/AuthService.java
+++
@@ -1,4 +0,0 @@
-package org.example.ticketmanagement.service;
-
-public interface AuthService {
-}
--- TicketManagement/src/main/java/org/example/ticketmanagement/service/impl/AuthServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package org.example.ticketmanagement.service.impl;
-
-import org.example.ticketmanagement.service.AuthService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthServiceImpl implements AuthService {
-}
